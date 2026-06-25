5d265ebc5266523d5a8cc484f50e9f1f08139a9f
devyashrasela <devyashrasela@gmail.com>
2026-06-25 21:57:37 +0530

    footer updated

--- components/Published.tsx
+++ components/Published.tsx
@@ -16,7 +16,7 @@ export default function Published() {
 
   return (
     <div className="bg-[#3f5c4a] py-12 px-1 xl:px-16 mt-10 max-w-[1920px] min-h-[550px] md:min-h-[600px] mx-auto">
-      
+
       {/* Heading */}
       <motion.div
         variants={fadeUp}
@@ -35,105 +35,106 @@ export default function Published() {
       </motion.div>
 
       {/* Cards */}
-<motion.div
-  variants={fadeUp}
-  initial="hidden"
-  whileInView="show"
-  viewport={{ once: true }}
-  className="flex md:grid md:grid-cols-2 gap-6 mt-10 max-w-[1000px] mx-auto overflow-x-auto md:overflow-visible snap-x snap-mandatory"
->
-<motion.div
-  variants={fadeUp}
-  whileHover={{ scale: 1.02 }}
-  className="w-[300px] md:w-[420px] flex-shrink-0 md:w-auto snap-start bg-white rounded-3xl p-6 flex gap-4 items-start"
->
-            {/* Logo */}
-            <Image
-              src="/22.png"
-              alt="logo"
-              width={80}
-              height={80}
-              className="w-10 h-10 md:w-20 md:h-20 rounded-md hidden md:block"
-            />
+      <motion.div
+        variants={fadeUp}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+        className="flex md:grid md:grid-cols-2 gap-6 mt-10 max-w-[1000px] mx-auto overflow-x-auto md:overflow-visible snap-x snap-mandatory"
+      >
+        <motion.div
+          variants={fadeUp}
+          whileHover={{ scale: 1.02 }}
+          className="w-[300px] md:w-[420px] flex-shrink-0 md:w-auto snap-start bg-white rounded-3xl p-6 flex gap-4 items-start"
+        >
+          {/* Logo */}
+          <Image
+            src="/22.png"
+            alt="logo"
+            width={80}
+            height={80}
+            className="w-10 h-10 md:w-20 md:h-20 rounded-md hidden md:block"
+          />
 
-            {/* Content */}
-            <div className="flex flex-col space-y-2  text-[#544120]">
-              <div className="md:hidden flex">
-                     <Image
-              src="/22.png"
-              alt="logo"
-              width={80}
-              height={80}
-              className="w-10 h-10 md:w-20 md:h-20 rounded-md"
-            />
-                <h3 className="font-semibold text-[10px] md:text-sm">
-                    Unheeded wisdom and a materialistic superpower
-                </h3>
-              </div>
-              <h3 className="font-semibold text-[10px] md:text-sm hidden md:block">
-                    Unheeded wisdom and a materialistic superpower
+          {/* Content */}
+          <div className="flex flex-col space-y-2  text-[#544120]">
+            <div className="md:hidden flex">
+              <Image
+                src="/22.png"
+                alt="logo"
+                width={80}
+                height={80}
+                className="w-10 h-10 md:w-20 md:h-20 rounded-md"
+              />
+              <h3 className="font-semibold text-[10px] md:text-sm">
+                Unheeded wisdom and a materialistic superpower
               </h3>
-
-              <p className="text-[8px] md:text-[10px] leading-relaxed">
-                    Spirituality has been made unnecessarily serious. Priests, monks, and moralists have turned it into a grave business, as if truth can only be reached with a long face. But existence is not serious. Existence is playful.
-              </p>
-
-              <span className="text-[8px] md:text-sm mt-5 md:mt-10 lg:mt-2">
-                <Link className="cursor-pointer underline"   
-                      target="_blank"
-                      rel="noopener noreferrer" href="https://timesofindia.indiatimes.com/blogs/toi-edit-page/even-victors-find-no-joy-in-aftermath-of-war/">
-                  Read on Speaking Tree
-                </Link>
-              </span>
             </div>
-          </motion.div>
-<motion.div
-  variants={fadeUp}
-  whileHover={{ scale: 1.02 }}
-  className="w-[300px] md:w-[420px] flex-shrink-0 md:w-auto snap-start bg-white rounded-3xl p-6 flex gap-4 items-start"
->
-            {/* Logo */}
-            <Image
-              src="/22.png"
-              alt="logo"
-              width={80}
-              height={80}
-              className="w-10 h-10 md:w-20 md:h-20 rounded-md hidden md:block"
-            />
+            <h3 className="font-semibold text-[10px] md:text-sm hidden md:block">
+              Unheeded wisdom and a materialistic superpower
+            </h3>
 
-            {/* Content */}
-            <div className="flex flex-col space-y-2  text-[#544120]">
-              <div className="md:hidden flex">
-                        <Image
-              src="/22.png"
-              alt="logo"
-              width={80}
-              height={80}
-              className="w-10 h-10 md:w-20 md:h-20 rounded-md"
-            />
-                  <h3 className="font-semibold text-[10px] md:text-sm ">
-             Laughter: The forgotten doorway to divinity
+            <p className="text-[8px] md:text-[10px] leading-relaxed">
+              Spirituality has been made unnecessarily serious. Priests, monks, and moralists have turned it into a grave business, as if truth can only be reached with a long face. But existence is not serious. Existence is playful.
+            </p>
+
+            <span className="text-[8px] md:text-sm mt-5 md:mt-10 lg:mt-2">
+              <Link className="cursor-pointer underline"
+                target="_blank"
+                rel="noopener noreferrer" href="https://timesofindia.indiatimes.com/blogs/toi-edit-page/even-victors-find-no-joy-in-aftermath-of-war/">
+                Read on Speaking Tree
+              </Link>
+            </span>
+          </div>
+        </motion.div>
+        <motion.div
+          variants={fadeUp}
+          whileHover={{ scale: 1.02 }}
+          className="w-[300px] md:w-[300px] flex-shrink-0 md:w-auto snap-start bg-white rounded-3xl p-6 flex gap-4 items-start"
+        >
+          {/* Logo */}
+          <Image
+            src="/22.png"
+            alt="logo"
+            width={80}
+            height={80}
+            className="w-10 h-10 md:w-20 md:h-20 rounded-md hidden md:block"
+          />
+
+          {/* Content */}
+          <div className="flex flex-col space-y-2 text-[#544120]">
+            <div className="md:hidden flex">
+              <Image
+                src="/22.png"
+                alt="logo"
+                width={80}
+                height={80}
+                className="w-10 h-10 md:w-20 md:h-20 rounded-md"
+              />
+              <h3 className="font-semibold text-[10px] md:text-sm ">
+                Laughter: The forgotten doorway to divinity
               </h3>
 
-              </div>
-              <h3 className="font-semibold text-[10px] md:text-sm hidden md:block">
-             Laughter: The forgotten doorway to divinity
-              </h3>
-
-              <p className="text-[8px] md:text-[10px] leading-relaxed">
-                When Swami Vivekananda, Paramhansa Yogananda, Osho, and AC Bhaktivedanta Swami Prabhupada arrived in US, they carried with them not merely Eastern teachings, but a corrective wisdom meant for a civilisation rising rapidly in external power yet lacking inner anchorage.
-              </p>
-
-              <span className="text-[8px] md:text-sm mt-2">
-                <Link   className="cursor-pointer underline"   
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://timesofindia.indiatimes.com/blogs/toi-edit-page/laughter-the-forgotten-doorway-to-divinity/">
-                           Read on Speaking Tree
-                </Link>
-              </span>
             </div>
-          </motion.div>
+            <h3 className="font-semibold text-[10px] md:text-sm hidden md:block">
+              Laughter: The forgotten doorway to divinity
+            </h3>
+
+            <p className="text-[8px] md:text-[10px] leading-relaxed">
+              When Swami Vivekananda, Paramhansa Yogananda, Osho, and AC Bhaktivedanta Swami Prabhupada arrived in US, they carried with them not merely Eastern teachings, but a corrective wisdom meant for a civilisation rising rapidly in external power yet lacking inner anchorage.
+            </p>
+
+            <span className="text-[8px] md:text-sm mt-2">
+              <Link className="cursor-pointer underline"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://timesofindia.indiatimes.com/blogs/toi-edit-page/laughter-the-forgotten-doorway-to-divinity/">
+                Read on Speaking Tree
+              </Link>
+            </span>
+          </div>
+        </motion.div>
+        
       </motion.div>
     </div>
   );
